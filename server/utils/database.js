fc82bf834df1a0a347327d439e80538d59544c9d
Colby Bogle <60157343+seamaniac81@users.noreply.github.com>
2021-09-26 12:50:46 -0500

    Update database.js

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('loginDB', 'root', 'Cb06142001!!', {
+const sequelize = new Sequelize('loginDB', '*Insert SQL Database Username here*', '*Insert SQL Database password here*', {
     dialect: 'mysql',
     host: 'localhost',
 });
