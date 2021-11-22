1c969b050e3b637de93ae203ad02f798e90a75b8
wrowe002 <wrowe002@drury.edu>
2021-11-22 00:59:10 -0600

    Fixed btn that looked like the scroll bar

--- components/Student.js
+++ components/Student.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Button, View, Text, StyleSheet, Picker, TouchableOpacity, ActivityIndicator } from 'react-native';
-import { createAppContainer } from "react-navigation";
+import { Button, View, Text, StyleSheet, Picker, TouchableOpacity, ActivityIndicator,ScrollView, ScrollViewComponent } from 'react-native';
+import { createAppContainer, SafeAreaView } from "react-navigation";
 import { CardStyleInterpolators, createStackNavigator } from "react-navigation-stack";
+
 import axios from 'axios'
 import CredField from './CredField'
 
@@ -62,88 +63,112 @@ const Student = (props) => {
   }
 
   return (
-    <View style={styles.container}>
+    
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
       <View style={styles.InfoText}> 
         <Text style={styles.txt1}>Welcome Back {name}!</Text>
         <Text style={styles.txt2}>Please choose your desired major, minor, or certificate options from the list below:</Text>
         <Text style={styles.txt3}>NOTE: choose at least three credentials:  one major and two certificates.  One certificate can be replaced with a second major or a minor.</Text>
       </View>
-      <View style={styles.form2}>
-        <View style ={styles.pickerSet}> 
+
+      <View style= {styles.form} >
+      
+        <View>
         <Picker style={styles.picker} placeholder='Major' >
           <Picker.Item label="Select Major" value="" />
           <Picker.Item label="Computer Science: Game Development" value="CSGD" />
           <Picker.Item label="Computer Science: Software Engineering" value="CSSE" />
           <Picker.Item label="Mathematics" value="MATH" />
         </Picker>
-     
-        
-        <Picker style={styles.picker} placeholder='Minor'>
-          <Picker.Item label="Select Minor" value="" />
-          <Picker.Item label="Computer Science" value="CSCI" />
-          <Picker.Item label="Criminology" value="CRIM" />
-          <Picker.Item label="English" value="ENGL" />
-        </Picker>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntext}>Add Major</Text>
+        </TouchableOpacity>
+        </View>
+
        
-        
+      <View>
+
+     <Picker style={styles.picker} placeholder='Minor'>
+       <Picker.Item label="Select Minor" value="" />
+      <Picker.Item label="Computer Science" value="CSCI" />
+      <Picker.Item label="Criminology" value="CRIM" />
+      <Picker.Item label="English" value="ENGL" />
+      </Picker>
+
+      </View>
+      
+      <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntext}>Add Minor</Text>
+        </TouchableOpacity>
+
+        <View>
         <Picker style={styles.picker} placeholder='Certificate'>
           <Picker.Item label="Select Certificate" value="" />
           <Picker.Item label="Interactive Design" value="INTD" />
           <Picker.Item label="International Immersion" value="INTI" />
           <Picker.Item label="Ancients Alive: The Classics in Context" value="ANCA" />
         </Picker>
-       </View> 
-      </View>
-      <View style={styles.form}>
-        <TouchableOpacity style={styles.btn} onPress={onMajorAdd}>
-          <Text style={styles.btntext}>Add Major</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btntext}>Add Minor</Text>
-        </TouchableOpacity>
+        </View>
+
         <TouchableOpacity style={styles.btn}>
           <Text style={styles.btntext}>Add Certificate</Text>
         </TouchableOpacity>
+        
       </View>
+    
       <View style={styles.centered}>
         <Text style={[styles.message, { color: isError ? 'red' : '#F5F5F5' }]}>{message}</Text>
         <TouchableOpacity style={styles.btn} onPress={onSubmitHandler}>
           <Text style={styles.btntext}>Submit</Text>
         </TouchableOpacity>  
-      </View>
-    </View>
+      </View> 
+      </ScrollView>
+    </SafeAreaView>
+   
   )
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 5,
     backgroundColor: '#F5F5F5',
     width: '100%',
     aspectRatio : 1,
-    marginLeft: "0.5%"
   },
+
   centered: {
     justifyContent: 'center',
     alignItems: 'center',
     paddingTop: "2%"
   },
   
-  form: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: "100%"
+  form:{
+    marginLeft: '10%',
+    
   },
+
   btn: {
-    width: "20%",
+    width: "45%",
     backgroundColor: "crimson",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
     marginTop: "1%",
+    marginBottom: "0.9%",
+    marginLeft: '1%',
     flexShrink: 1
   },
+  btn2:{
+      width: "40%",
+      backgroundColor: "crimson",
+      borderRadius: 25,
+      height: '200%',
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
   btntext: {
     color: "white",
     fontWeight: 'bold',
@@ -151,7 +176,8 @@ const styles = StyleSheet.create({
   form2: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    
+    alignItems: 'center',
+    width: '100%'
   },
   load: {
     flex: 1,
@@ -162,10 +188,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   picker: {
-    flex: 0.25,
-    resizeMode: "contain",
-    marginRight: '1%',
-    
+    width: '60%'
     
   },
   pickerSet:{
