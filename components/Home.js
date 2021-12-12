1a54747f829b2341614cce2bf8a92be867b32b60
Vuyisile Mjimba <vmjimba@drury.edu>
2021-12-12 01:15:33 -0600

    Submit Button Error

--- components/Home.js
+++ components/Home.js
@@ -1,176 +1,169 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Dimensions, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import {View, StyleSheet, ScrollView, Text, Image} from 'react-native';
+import {FormBuilder} from 'react-native-paper-form-builder';
+import {useForm} from 'react-hook-form';
+import {Button, Surface, icon} from 'react-native-paper';
 import axios from 'axios';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import SignUp from './SignUp';
 
 const api = axios.create({
   baseURL: `http://localhost:3210`
 })
+const Home= (props) => {
+  
+  /* const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); */
+  
+  const {control, setFocus, handleSubmit} = useForm({
+    defaultValues: {
+      Email: '',
+      Password: '',
+    },
+    mode: 'onChange',
+  });
 
-const Home = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isError, setIsError] = useState(false);
-  const [message, setMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onSignUp = () => {
-    props.navigation.navigate('SignUp');
-  }
   const onForgotPass = () => {
     props.navigation.navigate('ForgotPass')
   }
-
-  const onSubmitHandler = () => {
-    setIsLoading(true);
-    api.post('/api/auth/login', {
-      'Email': email,
-      'Password': password
-    })
-      .then(function (response) {
-        if (response.status != 200) {
-          setIsError(true);
-          setMessage('Username/Password incorrect');
-        }
-        else {
-          if (response.data.Role == 'admin') {
-            props.navigation.navigate('Admin');
-            setTimeout(() => { setIsLoading(false); }, 3000);
-          }
-          else if (response.data.Role == 'advisor') {
-            props.navigation.navigate('Advisor');
-            setTimeout(() => { setIsLoading(false); }, 3000);
-          }
-          else {
-            props.navigation.navigate('Student');
-            setTimeout(() => { setIsLoading(false); }, 3000);
-          }
-        }
-      })
-      .catch(function (error) {
-        setTimeout(() => { setIsLoading(false); }, 1000);
-        console.log(error);
-        setIsError(true);
-        setMessage('Username/Password incorrect');
-      });
-  };
-
-  if (isLoading) {
-    return (
-      <View style={styles.load}>
-        <ActivityIndicator size='large' />
-      </View>
-    )
+  const onSignUp = () => {
+    props.navigation.navigate('SignUp');
   }
 
   return (
-    <View style={styles.container}>
-
+    <View style={styles.containerStyle}>
+      <ScrollView contentContainerStyle={styles.scrollViewStyle}>
+      <Surface style={styles.formContainer}>
+      <View style={styles.formBuild}>
       <Image
-          style={{width: "90%", height: "20%", resizeMode:"contain"  , marginTop: '2%', }}
+           style={{width: "90%", height: "30%", resizeMode:"contain"  , marginTop: '3%', marginBottom: '2%', alignSelf: 'center' }}
           source={{uri:'https://drury.edu/wp-content/uploads/files/brand_lounge/PrimaryFullColor.png'}}
-      />
-      {/* <Image source={require('../assets/RD Logos/drury.png')} style={{width: "38%", height: "20%", flex: 1.9, marginTop: '0.3%'}} /> */}
-      <View style={styles.loginFields}>
-      <View style={styles.inputView} >
-        <TextInput
-          style={styles.inputText}
-          placeholder="Email..."
-          placeholderTextColor="#003f5c"
-          onChangeText={setEmail} />
-      </View>
-      <View style={styles.inputView} >
-        <TextInput
-          style={styles.inputText}
-          secureTextEntry={true}
-          placeholder="Password..." secureTextEntry={true}
-          placeholderTextColor="#003f5c"
-          onChangeText={setPassword} />
-      </View>
-      </View>
-      <Text style={[styles.message, { color: isError ? 'red' : '#F5F5F5' }]}>{message}</Text>
-      <View style={styles.buttonsform}>
-        <TouchableOpacity style={styles.btn} onPress={() => onSignUp()}>
-          <Text style={styles.btntext}>Sign Up</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={onSubmitHandler}>
-          <Text style={styles.btntext}>Login</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.forgotPass}>
-      <TouchableOpacity onPress={() => onForgotPass()}>
-        <Text style={styles.forgot}>Forgot Password?</Text>
-      </TouchableOpacity>
-      </View>
+       /> 
+
+       
+        <FormBuilder
+          control={control}
+          setFocus={setFocus}
+          formConfigArray={[
+            {
+              type: 'email',
+              name: 'Email',
+              
+
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Email is required',
+                },
+              },
+              textInputProps: {
+                label: 'Email',
+              },
+              
+            },
+            {
+              type: 'password',
+              name: 'Password',
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Password is required',
+                },
+                minLength: {
+                  value: 5,
+                  message: 'Enter valid password',
+                },
+              },
+              textInputProps: {
+                label: 'Password',
+              },
+            },
+          ]}
+        />
+
+       
+        <Button
+        style={styles.btn}
+        mode={'contained'}
+        onPress={handleSubmit((values) => api.post('/api/auth/login', {
+          'Email': values.Email,
+          'Password': values.Password,
+          
+        })
+        .then(function (response) {
+          console.log("sent");
+          if (response.status != 201) {
+            setIsError(true)
+          }
+          else {
+            props.navigation.navigate('Home');
+          }
+          
+        })
+        .catch(function (error) {
+          console.log(error);
+          
+        })
+        
+        )}>
+
+        <Text style={styles.btnText}> Log In </Text>
+
+        </Button>
+
+        <Button
+        onPress={() => onSignUp()}
+        style={styles.btn}
+        >
+          <Text style={styles.btnText}>Sign Up</Text>
+
+        </Button>
+
+        <Button onPress={() => onForgotPass()} > <Text style={styles.forgotpass}> Forgot password? </Text> </Button>
+        </View>
+        </Surface>
+      </ScrollView>
     </View>
-  )
+  );
 }
 
-//Image styling components
-
 const styles = StyleSheet.create({
-  load: {
+  containerStyle: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'center',
+    width: '100%',
+    aspectRatio: 10/8
   },
-  message: {
-    fontWeight: 'bold',
-  },
-  container: {
+
+  scrollViewStyle: {
     flex: 1,
-    alignItems: 'center',
+    padding: '2%',
     justifyContent: 'center',
-    backgroundColor: '#F5F5F5',
-    flexDirection: "column"
   },
-  buttonsform: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    flex: 1,
-  },
-  inputView: {
-    width: "65%",
-    backgroundColor: "white",
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 10,
-    justifyContent: "center",
-    padding: 20
-  },
-  inputText: {
-    height: 50,
-    color: "black",
-  },
-  forgot: {
-    color: "blue",
-    fontSize: 15
-  },
+
   btn: {
-    width: '40%',
-    backgroundColor: "crimson",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 20,
-    
-    padding: 10,
+    backgroundColor: 'crimson',
+    marginBottom: '4%',
   },
-  btntext: {
-    color: "white",
-    fontWeight: 'bold',
+
+  btnText:{
+    color: 'white'
   },
- 
-  loginFields: {
-    flex: 2,
-    width: '80%',
-    justifyContent: 'center',
-    marginLeft: '26%'
+
+  forgotpass:{
+    color: 'blue'
   },
-  forgotPass:{
-    flex: 0.5,
+
+  formContainer:{
+    width: '50%',
+    height: '100%',
+    alignSelf: 'center'
   },
+
+  formBuild:{
+    width: '75%',
+    alignSelf: 'center'
+  }
 });
 
 export default Home;
